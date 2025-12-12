f0076a75fc5bd5359e0fbe680ed990fa355b6fac
Shaheer Mirza <83059821+Shaheer-Mirza@users.noreply.github.com>
2025-12-12 16:11:35 +0500

    Update setup script for Linux with new installations

--- setting-up-linux.sh
+++ setting-up-linux.sh
@@ -5,23 +5,40 @@ sudo apt update && sudo apt full-upgrade -y
 
 cd ~/Downloads
 
-#wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+#install google chrome
 
+#wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 #sudo apt install ./google-chrome-stable_current_amd64.deb -y
 
-wget https://files2.freedownloadmanager.org/6/latest/freedownloadmanager.deb
+#Install FDM
 
+wget https://files2.freedownloadmanager.org/6/latest/freedownloadmanager.deb
 sudo apt install ./freedownloadmanager.deb -y
 
-git clone https://github.com/meowcateatrat/elephant.git
+#Install elephant add on on fdm
 
+git clone https://github.com/meowcateatrat/elephant.git
 cd elephant/build
 chmod +x build-unix.sh
 ./build-unix.sh
-
 cp elephant.fda ~/Downloads/
-
 cd ~/Downloads
 
+#Lid Close Action
+
+sudo sh -c 'printf "\nHandleLidSwitch=ignore\nHandleLidSwitchExternalPower=ignore\nHandleLidSwitchDocked=ignore\n" >> /etc/systemd/logind.conf'
+sudo systemctl restart systemd-logind
+
+#install snap
+
+sudo apt install snapd
+sudo systemctl enable snapd.apparmor.service
+
+#Install Flathub
+
+sudo apt install flatpak
+sudo apt install gnome-software-plugin-flatpak
+sudo apt install plasma-discover-backend-flatpak
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
 
